c881980c184738d866e672798077326d895bb186
kiyodesu <kiyotomm99@gmail.com>
2025-02-18 21:29:58 +0900

    a

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar";
+
+const Header = () => {
+  return (
+    <nav className="flex justfiy-center gap-10 w-screen">
+      Home About Contact
+    </nav>
+  );
+};
+
+export default Header;
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+  return (
+    <section className="flex justfiy-center gap-10">Home About Contact</section>
+  );
+};
+
+export default NavBar;
